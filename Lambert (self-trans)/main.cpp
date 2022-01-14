945a101a3f04de3d37473f2fe1cde3348ee1e81a
Polly Joe <pollyjoe2003@gmai.com>
2022-01-14 15:16:10 +0800

    Initial Commit

---
+++ Lambert (self-trans)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lambert (self-trans)
+//
+//  Created by 周懿 on 2022/1/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
